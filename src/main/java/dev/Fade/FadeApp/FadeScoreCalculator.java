8f1724c1027e4a504ef7066bab5b2897567a40ca
Passifi <pascal.wigger@gmx.de>
2026-08-04 11:41:49 +0200

    DistanceScoretest

--- src/main/java/dev/Fade/FadeApp/FadeScoreCalculator.java
+++ src/main/java/dev/Fade/FadeApp/FadeScoreCalculator.java
@@ -1,18 +1,20 @@
 package dev.Fade.FadeApp;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
 public class FadeScoreCalculator {
+    public FadeScoreCalculator() {}
     private float calculateDistanceScore(Instant reference, Instant other) {
-       var result = reference.compareTo(other); 
-       return Math.abs(result);
+       Duration diff = Duration.between(reference, other);
+       return Math.abs(diff.toMinutes())+1;
     } 
     public float calculateScore(List<Instant> upvotesTimes, Instant now) {
         if(upvotesTimes.isEmpty()) return 0.0f; 
         float totalScore = 0.0f; 
         for (Instant instant : upvotesTimes) {
-            totalScore += 1.0/calculateDistanceScore(now, instant);
+            totalScore += calculateDistanceScore(now, instant);
         } 
         return totalScore;
     }
--- src/test/java/dev/Fade/FadeApp/FadeAppApplicationTests.java
+++ src/test/java/dev/Fade/FadeApp/FadeAppApplicationTests.java
@@ -2,6 +2,13 @@ package dev.Fade.FadeApp;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +19,7 @@ import jakarta.validation.constraints.AssertTrue;
 class FadeAppApplicationTests {
 	@Autowired
 	FadeService service;
+	FadeScoreCalculator scorer = new FadeScoreCalculator();
 	@Test
 	void contextLoads() {
 	}
@@ -19,7 +27,6 @@ class FadeAppApplicationTests {
 	void getFades() {
 		var result = service.getDisocveryFades().iterator();
 		assertTrue(result.hasNext());
-	
 	}
 
 	@Test 
@@ -27,6 +34,24 @@ class FadeAppApplicationTests {
 		var result = service.getUpvotesByFade(-1).iterator();
 		assertTrue(!result.hasNext());
 	
+	}
+	
+	@Test void manyAgainstCurrentVotes() {
+
+		Instant now = Instant.now();	
+		Instant old = now.minus(30,ChronoUnit.DAYS);
+		List<Instant> many = new ArrayList<Instant>();
+		
+		List<Instant> oneNew = new ArrayList<Instant>(){{add(now);}};
+		for(int i = 0;i < 1220; i++) {
+			many.add(old);
+		}
+		float manyScore = scorer.calculateScore(many, now);
+		float oneScore = scorer.calculateScore(oneNew, now);
+		System.out.println("Score for many votes:" + manyScore);
+		System.out.println("Score for one recent vote:" + oneScore);
+		
+		assertTrue(manyScore>oneScore);
 	}
 
 }
